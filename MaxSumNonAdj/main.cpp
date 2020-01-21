0d1f066f0a1da5896414272fe5f2a10534b66a9c
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-21 09:57:03 +0900

    max adjacent sum of non negative integers

--- MaxSumNonAdj/main.cpp
+++ MaxSumNonAdj/main.cpp
@@ -7,9 +7,23 @@
 //
 
 #include <iostream>
+#include <vector>
+
+int maxSubsetSumNoAdjacent(std::vector<int> array) {
+    if(array.size() == 0){
+        return 0;
+    }else if(array.size() == 1){
+        return array.at(1);
+    }
+    std::vector<int> resultArray = array;
+    resultArray[1] = std::max(array[0],array[1]);
+    for(int i = 2; i < array.size(); ++i){
+        resultArray[i] = std::max(resultArray[i-1],resultArray[i-2]+array[i]);
+    }
+    return resultArray[array.size() - 1];
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Sum of non adjascent numbers:"<<maxSubsetSumNoAdjacent({75,105,120,75,90,135}) << std::endl;
     return 0;
 }
