88f0eb870aa7cec32e701e7b82e2b66c35cf3b15
Adeeb Mohammed <adeeb2358@gmail.com>
2020-02-13 01:10:07 +0900

    sum of maximum non adjacent numbers

--- MaxSumNonAdj/main.cpp
+++ MaxSumNonAdj/main.cpp
@@ -5,15 +5,35 @@
 //  Created by adeeb mohammed on 19/01/20.
 //  Copyright © 2020 adeeb mohammed. All rights reserved.
 //
+// a function to find out the maximum sum of the adjasent numbers
 
 #include <iostream>
 #include <vector>
 
-int maxSubsetSumNoAdjacent(std::vector<int> array) {
+int maxSubsetSumNoAdjacent(std::vector<int> array){
+    if(array.size() == 0){
+        return 0;
+    }else if(array.size() ==  1){
+        return array.at(0);
+    }
+    int tempSum     = 0;
+    int prevSum     = array.at(0);
+    int greatestSum = std::max(array.at(0),array.at(1));
+    for(int i = 2; i < array.size(); ++i){
+        tempSum = greatestSum;
+        greatestSum = std::max(greatestSum,prevSum+array.at(i));
+        prevSum = tempSum;
+        
+    }
+    return greatestSum;
+}
+
+// time complexity is o(n) , space complexity is
+int maxSubsetSumNoAdjacentOrg(std::vector<int> array) {
     if(array.size() == 0){
         return 0;
     }else if(array.size() == 1){
-        return array.at(1);
+        return array.at(0);
     }
     std::vector<int> resultArray = array;
     resultArray[1] = std::max(array[0],array[1]);
@@ -25,5 +45,6 @@ int maxSubsetSumNoAdjacent(std::vector<int> array) {
 
 int main(int argc, const char * argv[]) {
     std::cout << "Sum of non adjascent numbers:"<<maxSubsetSumNoAdjacent({75,105,120,75,90,135}) << std::endl;
+    std::cout << "Sum of non adjascent numbers:"<<maxSubsetSumNoAdjacentOrg({75,105,120,75,90,135}) << std::endl;
     return 0;
 }
